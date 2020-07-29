f1e44c9671ce07e517ae5938f7ccf5cc253b44a5
Dillon Estrada <dillon.estrada55@gmail.com>
2020-07-28 23:49:47 -0500

    further functionality

--- scrape.py
+++ scrape.py
@@ -1,27 +1,44 @@
 import requests
 from bs4 import BeautifulSoup
+import re
 
 def get_zzounds_products(path):
     result = requests.get(path)
     src = result.content
     soup = BeautifulSoup(src, 'html5lib')
-    product_objects = find_all("div", class_="product-card")
+
+    product_objects = soup.find_all("div", class_="product-card")
+
+    products = []
+    for product in product_objects:
+        link = product.find("a", class_="prod-name")
+        text = link.text
+        href = link['href']
+        img_url = product.find("img", class_="product-img")['src']
+        price = product.find("div", class_="price-1").find("span", class_="price").text
+
+        regex = r'\S+'
+        filtered_price = re.search(regex, price).group(0)
+        products.append([text, href, img_url, filtered_price])
+        #print("Item: " + text + "\nLink: " + href + "\nImage URL: " + img_url + "\nPrice: " + filtered_price + "\n\n\n")
+
+    return products
+
+
+    """
     links = soup.find_all("a", class_="prod-name")
     products = []
 
     for link in links:
         if link.text:
             products.append([link.text, link['href']])
+        
     
     return products
-"""
-product_list = get_zzounds_products("https://www.zzounds.com/cat--3-Piece-Acoustic-Drum-Sets--5126")
-for product in product_list:
-    print(product)
+    """
 
-"https://www.zzounds.com/prodsearch?cat=2463&condition%5B0%5D=New&ob=pop&pa=33&form=search&key=cat&p=2"
+print(get_zzounds_products("https://www.zzounds.com/prodsearch?cat=2463&condition%5B0%5D=New&ob=az&pa=33&form=search&key=cat&p=2"))
 
-"""
 def get_multipage_zzounds_products(category="2463"):
     url = "https://www.zzounds.com/prodsearch?cat=" + category + "&condition%5B0%5D=New&ob=az&pa=33&form=search&key=cat"
 
@@ -41,10 +58,11 @@ def get_multipage_zzounds_products(category="2463"):
         
         products += get_zzounds_products(url)
     return products
-
+"""
 all_products = get_multipage_zzounds_products()
 file_object = open("test.txt", "w+")
 file_object.write(str(all_products))
 for product in all_products:
     print(product)
-        
+"""
+
