6bdbfba63ca1fe4fc4ad6ca412db7fdc4ab6eba2
Dillon Estrada <dillon.estrada55@gmail.com>
2020-08-07 22:15:52 -0500

    unimplement typeahead.js & add form functionality

--- app/routes.py
+++ app/routes.py
@@ -1,28 +1,52 @@
 from app import app
-from flask import Flask, request, Response, render_template, flash, redirect, url_for
+from flask import Flask, request, Response, render_template, flash, redirect, url_for, session
 from database.models import Product, User, RigList
 from mongoengine.queryset.visitor import Q
 from app.forms import LoginForm, RegistrationForm
 from mongoengine.errors import NotUniqueError
 from mongoengine import DoesNotExist
 from flask_login import logout_user, current_user, login_user
+import json
+from math import ceil
 
 
 @app.route('/')
 @app.route('/index')
 def index():
     return render_template('index.html')
-"""
-body = request.get_json()
-    print(body)
-    try:
-        product = Product(**body).save()
-    except NotUniqueError as e:
-        raise Exception(e)
-    
-    id = product.id
-    return {'id': str(id)}, 200
+
+@app.route('/products')
+def products():
     """
+    page_info = {
+            'query': request.args.get('q'),
+            'categories': request.args.get('categories'),
+            'page': request.args.get('page'),
+            'num_products': request.args.get('num_products')
+    }"""
+    """
+    if page_info['page']:
+        page_info['page'] = int(page_info['page'])
+    if page_info['num_products']:
+        page_info['num_products'] = int(page_info['num_products'])
+    """
+
+    try:
+        session['page_info']['page'] = int(request.args.get('page'))
+        page_info = session['page_info']
+        print(page_info)
+        return render_template('products.html', page_info=page_info)
+    except (AttributeError, TypeError) as e:
+        print("Error")
+
+        page_info = {
+            'query': "",
+            'categories': "all",
+            'page': 1,
+            'num_products': 20
+        }
+        return render_template('products.html', page_info=page_info)
+
 # USER AUTHENTICATION
 @app.route('/register', methods=['GET', 'POST'])
 def register():
@@ -62,11 +86,6 @@ def logout():
     return redirect(url_for('index'))
 
 # PRODUCT API
-@app.route('/products')
-def get_products():
-    products = Product.objects().to_json()
-    return Response(products, mimetype="application/json", status=200), render_template('products.html')
-
 @app.route('/products/<id>', methods=['GET'])
 def get_product(id):
     product = Product.objects.get(id=id).to_json()
@@ -77,13 +96,6 @@ def get_product(id):
 def get_group(id):
     group = Product.objects(instrument=id).to_json()
     return Response(group, mimetype="application/json", status=200)
-"""
-@app.route('/products/instrument/<id>/search', methods=['GET'])
-def get_group_from_query(id):
-    query = request.args.get('q')
-    product = Product.objects(Q(name__icontains=query) & Q(instrument=id)).to_json()
-    return Response(product, mimetype="application/json", status=200)
-"""
 
 @app.route('/products/search', methods=['GET'])
 def get_products_from_query():
@@ -94,15 +106,22 @@ def get_products_from_query():
 
     query = request.args.get('q')
     categories = request.args.get('categories')
-
-    if categories == "all":
-        print(categories)
-        product = Product.objects(name__icontains=query)[previous_index:index].to_json()
-    else:
-        product = Product.objects(Q(name__icontains=query) & Q(instrument=categories))[previous_index:index].to_json()
+    req = request.args.get('req')
+    if req == "products":
+        if categories == "all":
+            product = (Product.objects(name__icontains=query)[previous_index:index].to_json())
+            session['num_products'] = len(Product.objects(name__icontains=query))
+        else:
+            product = (Product.objects(Q(name__icontains=query) & Q(instrument=categories))[previous_index:index].to_json())
+            session['num_products'] = len(Product.objects(Q(name__icontains=query) & Q(instrument=categories)))
+        return Response(product, mimetype="application/json", status=200)
+    elif req == "page_info":
+        print(session['num_products'])
+        pages = ceil(session['num_products'] / number_per_page)
+        page_info = [json.dumps({'number_per_page': number_per_page, 'page': page, 'number_of_pages': pages, 'query': query, 'categories': categories, 'num_products': session['num_products']})]
+        session['page_info'] = json.loads(page_info[0])
+        return Response(page_info, mimetype="application/json", status=200)
     
-    
-    return Response(product, mimetype="application/json", status=200)
 
 @app.route('/product/search', methods=['GET'])
 def get_product_from_id():
--- app/static/js/main.js
+++ app/static/js/main.js
@@ -1,58 +1,64 @@
-$(document).ready(function(){
-  /* RADIO BUTTON FUNCTIONALITY */
-  instrument = "all"
-  $(".form-check > input").on("click", function(){
-    instrument = $(this).attr("value")
-  });
+/*
+<li class="page-item disabled"><a class="page-link" href="#" tabindex="-1" aria-disabled="true"><<</a></li>
+<li class="page-item disabled"><a class="page-link" href="#" tabindex="-1" aria-disabled="true"><</a></li>
+<li class="page-item active"><a class="page-link" href="/products?q={{ page_info.query }}&categories={{ page_info.categories }}&num_products={{ page_info.num_products }}&page={{ page_info.page }}">{{ page_info.page }}</a></li>
+<li class="page-item"><a class="page-link" href="/products?q={{ page_info.query }}&categories={{ page_info.categories }}&num_products={{ page_info.num_products }}&page={{ page_info.page + 1 }}">{{ page_info.page + 1 }}</a></li>
+<li class="page-item"><a class="page-link" href="/products?q={{ page_info.query }}&categories={{ page_info.categories }}&num_products={{ page_info.num_products }}&page={{ page_info.page + 2 }}">{{ page_info.page + 2 }}</a></li>
+<li class="page-item"><a class="page-link" href="/products?q={{ page_info.query }}&categories={{ page_info.categories }}&num_products={{ page_info.num_products }}&page={{ page_info.page + 1 }}" tabindex="-1" aria-disabled="true">></a></li>
+<li class="page-item"><a class="page-link" href="/products?q={{ page_info.query }}&categories={{ page_info.categories }}&num_products={{ page_info.num_products }}&page={{ page_info.number_of_pages }}" tabindex="-1" aria-disabled="true">>></a></li>
+*/
 
-  /* GET CURRENT PAGE FOR PAGINATION */
-  let page = getUrlParameter("page");
-  if (page == undefined){page = 1};
-  
-  /* TYPEAHEAD FUNCTIONALITY*/
-  $('.typeahead').typeahead({
-    minLength: 3,
-    menu: $('.f-search__suggestion'),
-    dataset: $('.f-dataset'),
-  },
-  {
-    name: 'products',
-    display: 'name',
-    async: true,
-    source: function(query, syncResults, asyncResults) {
-        $.get('/products/search?categories=' + instrument + '&q=' + query + '&page=' + page, function(data) {
-          console.log(data)
-          asyncResults(data);
-        });
-    },
-    limit: 'Infinity',
-    templates: {/*
-      suggestion: function(product){
-        //let id = product.name.replace(/\W/g, '').toLowerCase()
-        let id = product._id.$oid
-        let html = '<tr id="' + id + '"><th scope="row">' + product.name +'</th><td>' + product.price + '</td><td><a class="btn btn-primary" href="#" role="button">Add to list</a></div></li></td></tr>'
-        //let html = '<li id="' + id + '" class="list-group-item d-flex justify-content-between align-items-center product-search-item"><span class="small mr-4">' + product.price + '</span><span class="product-name">' + product.name + '</span><div class="btn-group ml-2" role="group" aria-label="..."><a class="btn btn-primary" href="#" role="button">Add to list</a></div></li>'
-        return html
-      },*/
-      header: function(products){
-        let html = '<table class="table"><thead class="thead-dark"><tr><th scope="col">Name</th><th scope="col">Price</th><th scope="col">Link</th></tr></thead><tbody class=".f-dataset">'
-        products.suggestions.forEach(function(product){
-          html += '<tr id="' + product._id.$oid + '"><th scope="row">' + product.name +'</th><td>' + product.price + '</td><td><a class="btn btn-primary btn-sm" href="#" role="button">Add to list</a></div></li></td></tr>'
-        })
-        return html
-      },
-      
-      footer: function(products){
-        let html = '</tbody></table>'
-        return html
-      },/*
-      pending: function(products){
-        $.get('/products/search?all=true', function(data) {
-          return JSON.stringify(data)
-        })
-      }*/
-    }
-  })
+/* GET URL PARAMETERS */
+var page_info = {}
+
+const urlParams = ["page", "categories", "q", "num_products"]
+let urlJson = {}
+urlParams.forEach(function(param){
+  urlJson[param] = getUrlParameter(param)
+})
+if (urlJson){
+  instrument = urlJson["categories"]
+  page_info['page'] = urlJson["page"]
+  page_info['query'] = urlJson['q']
+  page_info['num_products'] = urlJson['num_products']
+} else {
+  instrument = "all";
+  page_info.page = 1
+  page_info.query = ""
+  page_info.num_products = 0
+}
+
+/*let default_products = new Bloodhound({
+  queryTokenizer: Bloodhound.tokenizers.whitespace,
+  datumTokenizer: Bloodhound.tokenizers.whitespace,
+  identify: function(product) { return product._id.$oid },
+  local: getProductsFromQuery(instrument, "", page_info['page']),
+  remote: {
+    wildcard: '%QUERY',
+    url: '/products/search?req=products&categories=' + instrument + '&q=%QUERY&page=' + page_info['page']
+  }
+})*/
+
+$(document).ready(function(){
+  /*
+  $('ul.pagination > li.page-item').click(function(event){
+    console.log("TTTTTEEEESDTST")
+    $('#page-input').val = $(this).text()
+    $('#filter-form').submit();
+    event.preventDefault()
+  })*/
+/*
+  initializeTypeahead();
+  $('.typeahead').typeahead('val', urlJson['q']).focus()
+
+*/
+ 
+
+ /*$.ajax({url: '/products/search?req=page_info&categories=' + instrument + '&q=' + query + '&page=' + page, async: true, success: function(data){
+  page_info = data;
+}});*/
+
+ // $('.typeahead').typeahead('val', urlJson["q"])
   /* ADD ITEMS TO LIST */
   /* <div class="image-parent"></div> */
   /* <img src="' + product.image_url + '" class="img-thumbnail img-fluid ml-4" alt="' + product.name + '"> */
@@ -73,7 +79,16 @@ $(document).ready(function(){
 
 });
 
-const getUrlParameter = function getUrlParameter(sParam) {
+
+function getProductsWithDefaults(query="", sync, async) {
+  default_products.clear();
+  default_products.local = getProductsFromQuery(instrument, query, page_info['page'], false);
+  default_products.remote.url = '/products/search?req=products&categories=' + instrument + '&q=%QUERY&page=' + page_info['page']
+  default_products.initialize(true);
+  return default_products
+}
+
+function getUrlParameter(sParam) {
   let sPageURL = window.location.search.substring(1),
       sURLVariables = sPageURL.split('&'),
       sParameterName,
@@ -87,4 +102,50 @@ const getUrlParameter = function getUrlParameter(sParam) {
       } 
   }
 };
-    
+
+function getProductsFromQuery(instrument, query, page, async=true){
+  let dataArr = [];
+  $.ajax({url: '/products/search?req=products&categories=' + instrument + '&q=' + query + '&page=' + page, async: async, success: function(data){
+    dataArr = data
+    $.ajax({url: '/products/search?req=page_info&categories=' + instrument + '&q=' + query + '&page=' + page, async: async, success: function(data){
+      page_info = data;
+    }});
+  }})
+  return dataArr
+}
+
+/* TYPEAHEAD FUNCTIONALITY
+function initializeTypeahead() {
+  $('.typeahead').typeahead({
+    minLength: 0,
+    menu: $('.f-search__suggestion'),
+    dataset: $('.f-dataset'),
+  },
+  {
+    name: 'products',
+    display: 'name',
+    async: true,
+    source: getProductsWithDefaults /*function(query, syncResults, asyncResults) {
+        $.ajax({url: '/products/search?req=products&categories=' + instrument + '&q=' + query + '&page=' + page, async: true, success: function(data){
+          asyncResults(data)
+          $.ajax({url: '/products/search?req=page_info&categories=' + instrument + '&q=' + query + '&page=' + page, async: true, success: function(data){
+            page_info = data;
+          }});
+        }});
+    },
+    limit: 'Infinity',
+    templates: {
+      header: function(products){
+        let html = '<table class="table"><thead class="thead-dark"><tr><th scope="col">Name</th><th scope="col">Price</th><th scope="col">Link</th></tr></thead><tbody class=".f-dataset">'
+        products.suggestions.forEach(function(product){
+          html += '<tr id="' + product._id.$oid + '"><th scope="row">' + product.name +'</th><td>' + product.price + '</td><td><a class="btn btn-primary btn-sm" href="#" role="button">Add to list</a></div></li></td></tr>'
+        })
+        return html
+      },
+      footer: function(products){
+        let html = '</tbody></table>'
+        return html
+      },
+    }
+  })
+};*/
